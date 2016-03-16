d0a43ef4195988f027ede700861e4d8032f6c5fb
hafen <rhafen@gmail.com>
2016-03-16 12:24:30 -0700

    Update housing docs

--- R/housing.R
+++ R/housing.R
@@ -1,6 +1,6 @@
 #' Housing data
 #'
-#' Monthly median home listing, sale price per square foot, and number of units sold for 2984 counties in the contiguous United States From 2008 to January 2016, harvested from Quandl's Zillow Housing Data \url{https://www.quandl.com/collections/housing}.
+#' Monthly median home listing, sale price per square foot, and number of units sold for 2984 counties in the contiguous United States From 2008 to January 2016, harvested from Quandl's Zillow Housing Data \url{https://www.quandl.com/collections/housing}.  Provided from Quandl with rights to distribute without restriction.
 #'
 #' List price data is the most complete.  Many counties are missing sale price and number of units sold.  Only counties with at least one non-NA listing price entry are included in the data.  Also, the data only represents counties in the contiguous United States (Alaska, Hawaii, Puerto Rico, and Virgin Islands are not included).  A few major outliers in sale prices in small counties have been removed.
 #'
